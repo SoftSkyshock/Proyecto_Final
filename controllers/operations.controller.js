34caf2bf083886a1b10dc0bdb5b88f86fca4f1fc
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2021-12-15 17:50:30 -0600

    feat: IN-8 Add functionality in operations controller

--- controllers/operations.controller.js
+++ controllers/operations.controller.js
@@ -1,17 +1,42 @@
 module.exports = {
+    getAll: function (req, res) {
+        res.status(201)
+        .json({
+            'options': {
+                'suma': '/operations/sum',
+                'resta': '/operations/substract',
+                'multiplicacion': '/operations/multiply',
+                'division': '/operations/divide'
+            }
+        })
+        .send();
+    },
     sum: function (req, res) {
-        res.status(201).send();
+        const {a, b} = req.body
+        res.status(201).json({
+            'suma': `${a} + ${b} = ${parseFloat(a) + parseFloat(b)}`
+        });
     },
 
     substract: function (req, res) {
-        res.status(201).send();
+        const {a, b} = req.body
+        res.status(201).json({
+            'resta': `${a} - ${b} = ${parseFloat(a) - parseFloat(b)}`
+        });
     },
 
     multiply: function (req, res) {
-        res.status(201).send();
+        const {a, b} = req.body
+        res.status(201).json({
+            'multiplicacion': `${a} x ${b} = ${parseFloat(a) * parseFloat(b)}`
+        });
     },
 
     divide: function (req, res) {
-        res.status(201).send();
+        const {a, b} = req.body
+        const result = parseFloat(a) / parseFloat(b)
+        res.status(201).json({
+            'division': `${a} / ${b} = ${result.toFixed(2)}`
+        });
     }
 };

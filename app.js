2dd03210622d7507f732a9b6925f7964344498c3
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2021-12-15 17:59:25 -0600

    fix: IN-6 Add routes and uncomment

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var express = require('express');
+const express = require('express');
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var tasksRouter = require('./routes/tasks');
 var authRouter = require('./routes/auth');
 var paymentRouter = require('./routes/payment');
-
+var shipmentRouter = require('./routes/shipment');
+var operationRouter = require('./routes/operations');
+var dogstatsd = new StatsD();
 var app = express();
+
 
 // view engine setup
 app.set('view engine', 'jade');
-
+app.use(Sentry.Handlers.errorHandler());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 app.use('/tasks', tasksRouter);
-//app.use('/payment', paymentRouter);
-
+app.use('/payment', paymentRouter);
+app.use('/shipment', shipmentRouter);
+app.use('/operations', operationRouter);
 
 module.exports = app;

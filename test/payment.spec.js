f8c03056478dbabbf1fd41898a8bf418c81d4056
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2021-12-15 17:03:16 -0600

    fix: IN-16 Comment line 33

--- test/payment.spec.js
+++ test/payment.spec.js
@@ -9,7 +9,7 @@ const PAYMENT_FILE_PATH = path.resolve('./payment-generated.txt');
 const {Request, Response} = require('./mock');
 const utils = require('./utils');
 const {uniq} = require('lodash');
-const Promise = require('bluebird');
+const os = require('os')
 
 chai.use(chaiHttp);
 
@@ -30,9 +30,9 @@ describe('payment check', () => {
         if (agent) {
             agent.close();
         }
-        done();
+        // done();
         utils.removeFile(PAYMENT_FILE_PATH)
-          .then(() => done() )
+        .then(() => done() )
     });
 
     it('Should generate an random price', (done) => {
@@ -68,6 +68,46 @@ describe('payment check', () => {
             .get('/payment/promos')
             .then(promos => {
                 promos.body.length.should.eql(5);
+                done();
+            })
+    });
+    /* Nuevos test */
+    it('Should return oldContent', done => {
+        chai.request(server)
+            .post('/payment/apply-discount')
+            .send({ discount: 20})
+            .then(promos => {
+                promos.body.should.have.property('oldContent');
+                done();
+            })
+    });
+
+    it('Should return newContent', done => {
+        chai.request(server)
+            .post('/payment/apply-discount')
+            .send({ discount: 20})
+            .then(promos => {
+                promos.body.should.have.property('newContent');
+                done();
+            })
+    });
+
+    it('Should return prices with discount aplied or empty data', done => {
+        chai.request(server)
+            .post('/payment/apply-discount')
+            .send({ discount: 20})
+            .then(promos => {
+                if (promos.body.oldContent !== '' && promos.body.newContent !== '') {
+                    const old = promos.body.oldContent.split(os.EOL)
+                    const newc = promos.body.newContent.split(os.EOL)
+                    old.forEach((o, index) => {
+                        o.should.not.equal(newc[index])
+                    })
+                } else {
+                    promos.body.oldContent.length.should.eql(0)
+                    promos.body.newContent.length.should.eql(0)
+                }
+                // promos.body.should.have.property('newContent');
                 done();
             })
     });

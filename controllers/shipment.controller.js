191991a1e2c6f38b19ffb39db2247c1770773451
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2021-12-15 18:02:50 -0600

    feature: IN-12 Add functionality

--- controllers/shipment.controller.js
+++ controllers/shipment.controller.js
@@ -1,13 +1,34 @@
 //deberas de utilizar faker para generar los datos
+const faker = require('faker');
+
 module.exports = {
     createShipment: function (req, res) {
+        dogstatsd.increment('shipment.createShipment');
         //debera de simular un envio con dirección un precio y una persona con sus datos
-        res.status(201).send();
+        const name = faker.name.firstName()
+        const lastName = faker.name.lastName()
+        const address = `${faker.address.streetAddress(true)}, ${faker.address.zipCode()}, ${faker.address.city()}, ${faker.address.state()}}`
+        res
+        .status(201)
+        .json({
+            'address': address,
+            'price': faker.finance.amount(),
+            'user': {
+                'name': name,
+                'lastName': lastName,
+                'phone': faker.phone.phoneNumber(),
+                'email': faker.internet.email(name, lastName, 'solotrucking.com')
+            }
+        });
     },
     changeStatus: function (req, res) {
+        dogstatsd.increment('shipment.changeStatus');
         //Debera de retornar una dirección random
+        const address = `${faker.address.streetAddress(true)}, ${faker.address.zipCode()}, ${faker.address.city()}, ${faker.address.state()}}`
         // codigo de respuesta 201
         // data la direcciòn random
-        res.json({addres: ""});
+        res
+            .status(201)
+            .json({address: address})
     },
 };
